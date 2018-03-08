49c0f24af79a6d0c635a892cc833ad33252e7507
User <user@users-macbook.local>
2018-03-08 12:44:24 +0000

    Frome1

---
+++ FacebookPage1Controller.swift
@@ -0,0 +1,33 @@
+//
+//  FacebookPage1Controller.swift
+//  Frome Town
+//
+//  Created by User on 08/03/2018.
+//  Copyright © 2018 Dan Turner. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class FacebookPage1Controller: UIViewController, WKUIDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration(); webConfiguration
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let myURL = URL(string: "https://www.facebook.com/FromeTownFC/")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+        
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
