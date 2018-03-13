b8d7c737102a050240551760d063447818a54a3d
User <user@users-macbook.local>
2018-03-13 10:07:54 +0000

    Frome1

--- LeagueTableController.swift
+++ LeagueTableController.swift
@@ -1,34 +1,33 @@
 //
-//  Radio1ViewController.swift
+//  LeagueTableController.swift
 //  Frome Town
 //
-//  Created by User on 08/03/2018.
+//  Created by User on 12/03/2018.
 //  Copyright © 2018 Dan Turner. All rights reserved.
 //
 
 import UIKit
 import WebKit
 
-class Radio1ViewController: UIViewController, WKUIDelegate {
+class LeagueTableController: UIViewController, WKUIDelegate {
     
     var webView: WKWebView!
     
     override func loadView() {
-        
         let webConfiguration = WKWebViewConfiguration(); webConfiguration
         webView = WKWebView(frame: .zero, configuration: webConfiguration)
         webView.uiDelegate = self
         view = webView
     }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let myURL = URL(string: "http://mixlr.com/frome-town-fc--2")
+        let myURL = URL(string: "http://www.evostikleaguesouthern.co.uk/match-info/tables")
         let myRequest = URLRequest(url: myURL!)
         webView.load(myRequest)
+        
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-}
+    }
 }
