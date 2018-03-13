cf9f9c3e2f0994bc66a876ad596c25d0796e80ac
User <user@users-macbook.local>
2018-03-13 12:23:03 +0000

    Frome1

---
+++ FromeTownRobinsTwitter1Controller.swift
@@ -0,0 +1,36 @@
+//
+//  FromeTownRobinsTwitter1Controller.swift
+//  Frome Town
+//
+//  Created by User on 13/03/2018.
+//  Copyright © 2018 Dan Turner. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class FromeTownRobinsTwitter1Controller: UIViewController, WKUIDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration(); webConfiguration
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let myURL = URL(string: "https://twitter.com/frometownrobins?lang=en")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+        
+    }
+    
+    override func didReceiveMemoryWarning(){
+        super.didReceiveMemoryWarning()
+    }
+}
+
