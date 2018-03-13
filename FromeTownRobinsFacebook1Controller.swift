23e1c23d9bbfabced98cfb9c0fdd3bb87d75c8d6
User <user@users-macbook.local>
2018-03-13 12:34:09 +0000

    Frome1

---
+++ FromeTownRobinsFacebook1Controller.swift
@@ -0,0 +1,35 @@
+//
+//  FromeTownRobinsFacebook1Controller.swift
+//  Frome Town
+//
+//  Created by User on 13/03/2018.
+//  Copyright © 2018 Dan Turner. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class FromeTownRobinsFacebook1Controller: UIViewController, WKUIDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration(); webConfiguration
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let myURL = URL(string: "https://www.facebook.com/FromeTownRobins/")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
---
+++ Instagram1ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  Instagram1ViewController.swift
+//  Frome Town
+//
+//  Created by User on 13/03/2018.
+//  Copyright © 2018 Dan Turner. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class Instagram1ViewController: UIViewController, WKUIDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration(); webConfiguration
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let myURL = URL(string: "https://www.instagram.com/frometownfcofficial/")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
